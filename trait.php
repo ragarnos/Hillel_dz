8474875d09fe74590cabfe93f5558efc21fa5a19
Eduard Vasyliev <96519743+ragarnos@users.noreply.github.com>
2022-06-01 10:44:35 +0300

    retake

--- trait.php
+++ trait.php
@@ -1,30 +1,45 @@
 <?php
-trait Trait1 {
-    public function method() {
-        echo '1 ';
+trait Trait1
+{
+    public function method()
+    {
+        echo 1;
     }
 }
 
-trait Trait2 {
-    public function method1() {
-        echo '2 ';
+trait Trait2
+{
+    public function method()
+    {
+        echo 2;
     }
 }
-trait Trait3 {
-    public function method2() {
-        echo '3 ';
+
+trait Trait3
+{
+    public function method()
+    {
+        echo 3;
     }
 }
+
 
 class Test {
-    use Trait1, Trait2, Trait3;
+    use Trait1, Trait2, Trait3 {
+        Trait2::method insteadof Trait1;
+        Trait3::method insteadof Trait2;
+        Trait1::method as Trait1;
+        Trait2::method as Trait2;
+        Trait3::method as Trait3;
+    }
 }
+
 function getSum()
 {
     $o = new Test();
-    $o->method();
-    $o->method1();
-    $o->method2();
+    $o->Trait1();
+    $o->Trait2();
+    $o->Trait3();
 }
 echo getSum();
 ?>
