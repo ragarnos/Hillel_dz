6440ca27ccbec0f679684c94b384523a5765a8bb
Eduard Vasyliev <96519743+ragarnos@users.noreply.github.com>
2022-06-20 21:15:33 +0300

    Creation Pattern

--- index.php
+++ index.php
@@ -1,44 +1,71 @@
 <?php
 
-class Product
+interface Pay
 {
-    public function get($name)
-    {
-        // 
-    }
+    public function getAmount();
+}
+abstract class PMethod
+{
+    abstract public function newPay(): Pay;
 
-    public function set($name, $val)
+    public function totalPay()
     {
-        // 
+        $Pay = $this->newPay();
+        return $Pay->getAmount();
     }
 }
-class FactoryProduct
+class PrivatPay implements Pay
 {
-    public function save(Product $product)
-    {
-        // 
-    }
+    private int $amount;
 
-    public function update(Product $product)
+    public function __construct($amount)
     {
-        // 
+        $this->amount = $amount;
     }
-
-    public function delete(Product $product)
+    public function getAmount(): int
     {
-        // 
+        return $this->amount;
     }
-
-    public function show(Product $product)
+}
+class MonoPay implements Pay
+{
+    private int $amount;
+    public function __construct($amount)
     {
-        // 
+        $this->amount = $amount;
+    }
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+}
+class PrivatPayM extends PMethod
+{
+    private int $amount;
+    public function __construct($amount)
+    {
+        $this->amount = $amount;
+    }
+    public function newPay(): Pay
+    {
+        return new PrivatPay($this->amount);
+    }
+}
+class MonoPayM extends PMethod
+{
+    private int $amount;
+    public function __construct($amount)
+    {
+        $this->amount = $amount;
+    }
+    public function newPay(): Pay
+    {
+        return new MonoPay($this->amount);
     }
 }
 
-class Echo_Product
-{
-    public function view(Product $product)
-    {
-        // 
-    }
-}
+$pay = new PrivatPayM(560);
+echo $pay->totalPay();
+$pay = new MonoPayM(1920);
+echo '<br>';
+echo $pay->totalPay();
